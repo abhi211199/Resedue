ea3b9cd5d17cbd050221ac0bbd5151520ecda985
Abhishek Garain <abhi211199@gmail.com>
2018-11-21 23:51:30 +0530

    Added input in database method!

---
+++ app/src/main/java/com/example/resedue/resedue/form.java
@@ -0,0 +1,96 @@
+package com.example.resedue.resedue;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class form extends AppCompatActivity {
+
+    String[] country = {"Please Enter choice of selling agent", "Government", "Private"};
+    EditText name,add,wt,sell,mob,id;
+    FirebaseDatabase database;
+    users user;
+    String s,key;
+    DatabaseReference ref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        name=(EditText)findViewById(R.id.name);
+        add=(EditText)findViewById(R.id.address);
+        wt=(EditText)findViewById(R.id.wt);
+        mob=(EditText)findViewById(R.id.mob);
+        id=(EditText)findViewById(R.id.id);
+        Spinner spin = (Spinner) findViewById(R.id.sell);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(aa);
+        database=FirebaseDatabase.getInstance();
+        user=new users();
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                s=country[position];
+//                Toast.makeText(getApplicationContext(),s.toString(), Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    //getting values of form
+    public void getVal()
+    {
+        user.setAddress(name.getText().toString());
+        user.setMob(mob.getText().toString());
+        user.setName(name.getText().toString());
+        user.setWt(wt.getText().toString());
+        user.setSell(s.toString());
+    }
+
+    //adding values in database
+    public void add(View view)
+    {
+        final Date currentTime = Calendar.getInstance().getTime();
+        getVal();
+        database=FirebaseDatabase.getInstance();
+        ref=database.getReference("users");
+        key = ref.push().getKey();
+//        Toast.makeText(this,key,Toast.LENGTH_LONG).show();
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                getVal();
+                ref.child("123").child(key).setValue(user);
+                Toast.makeText(form.this,"success".toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/resedue/resedue/users.java
@@ -0,0 +1,84 @@
+package com.example.resedue.resedue;
+
+public class users{
+
+    private String name;
+    private String address;
+    private String mob;
+    private String id;
+    private String wt;
+    private String sell;
+    private String ref;
+
+    public users(String name, String address, String mob, String id, String wt, String sell, String ref) {
+        this.name = name;
+        this.address = address;
+        this.mob = mob;
+        this.id = id;
+        this.wt = wt;
+        this.sell = sell;
+        this.ref = ref;
+    }
+
+    public users() {
+
+        }
+
+        public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getWt() {
+        return wt;
+    }
+
+    public void setWt(String wt) {
+        this.wt = wt;
+    }
+
+    public String getSell() {
+        return sell;
+    }
+
+    public void setSell(String sell) {
+        this.sell = sell;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+
+}
